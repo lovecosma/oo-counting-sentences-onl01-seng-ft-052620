7382eddc4bcb234638051645ebbd9cd754e8892d
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-07 05:59:15 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?(".")
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
-
+  sentence = 0
+  array = self.split
+  array.each do |phrase|
+  sentence += 1 if phrase.sentence? || phrase.exclamation? || phrase.question?
   end
+  sentence
+    binding.pry
+  end
+
 end
